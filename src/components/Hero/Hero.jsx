80ff41e6a6e6a8d91d5b30c566fe005c00ee7572
mouralph-del <mouralph@gmail.com>
2026-08-31 12:47:12 -0300

    Refine Home hero composition

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -67,22 +67,26 @@ function Hero() {
           <title id="hero-photo-title">Thaís Santos em retrato profissional</title>
           <defs>
             <clipPath id="heroPhotoClip" clipPathUnits="userSpaceOnUse">
-              <path d="M 340 0 C 230 160 240 420 180 640 C 120 820 50 940 0 1000 L 1000 1000 L 1000 0 Z" />
+              <path d="M 270 0 C 185 180 205 420 145 620 C 92 800 38 930 0 1000 L 1000 1000 L 1000 0 Z" />
             </clipPath>
           </defs>
 
           <path
             className="hero-photo__accent"
-            d="M 280 0 C 170 160 185 420 125 640 C 70 820 20 940 -40 1000 L 80 1000 C 135 940 200 820 255 640 C 315 420 305 160 420 0 Z"
+            d="M 232 0 C 145 180 160 420 100 620 C 50 800 8 930 -24 1000 L 28 1000 C 68 930 122 800 174 620 C 235 420 218 180 302 0 Z"
           />
 
-          <image
-            href={thaisHeroPhoto}
+          <foreignObject
             width="1000"
             height="1000"
-            preserveAspectRatio="xMidYMid slice"
             clipPath="url(#heroPhotoClip)"
-          />
+          >
+            <img
+              className="hero-photo__image"
+              src={thaisHeroPhoto}
+              alt=""
+            />
+          </foreignObject>
         </svg>
       </figure>
 
